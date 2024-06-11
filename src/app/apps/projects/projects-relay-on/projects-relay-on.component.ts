664e1eef5c4a27f36184e75acc415d3b0f1a4900
DevKevvz <kfeliz@sdm.com.do>
2024-06-11 15:23:29 -0400

    Adding do-validator to projects

--- src/app/apps/projects/projects-relay-on/projects-relay-on.component.ts
+++ src/app/apps/projects/projects-relay-on/projects-relay-on.component.ts
@@ -8,94 +8,116 @@ import { Component } from '@angular/core';
 export class ProjectsRelayOnComponent {
   relayOn = [
     {
+      image: '../../../../assets/images/blog/do-validator.png',
+      field: 'do-validator package',
+      fieldText: 'Develop in TypeScript, do-validator is a package that offers a simple and efficient way to validate Dominican Republic ID numbers.',
+      link: 'https://www.npmjs.com/package/do-validator',
+      btnText: 'View Package'
+    },
+    {
       image: '../../../../assets/images/blog/portfolio.png',
       field: 'This Portfolio',
       fieldText: 'Professional portfolio that effectively showcases the projects, and achievements.',
-      link: 'https://github.com/DevKevs/portfolio'
+      link: 'https://github.com/DevKevs/portfolio',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/blog/products.jpg',
       field: 'Store Management',
       fieldText: 'Develop a Flutter app for small businesses to manage their store operations efficiently.',
-      link: 'https://github.com/DevKevs/products-app'
+      link: 'https://github.com/DevKevs/products-app',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/blog/movie.jpg',
       field: 'IMdb MovieApp',
       fieldText: 'Develop Movie App that provides users to explore, discover, and keep track of their favorite movies.',
-      link: 'https://github.com/DevKevs/flutter-movieApp'
+      link: 'https://github.com/DevKevs/flutter-movieApp',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'TokTik Flutter',
       fieldText: 'Flutter-based TikTok app clone designed for watching 16:9 videos.',
-      link: 'https://github.com/DevKevs/toktik'
+      link: 'https://github.com/DevKevs/toktik',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'Yes No App',
       fieldText: 'Flutter chat app featuring a bot that responds to yes or no questions.',
-      link: 'https://github.com/DevKevs/yes-no-app-flutter'
+      link: 'https://github.com/DevKevs/yes-no-app-flutter',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'QR Scanner',
       fieldText: 'Flutter QR scanner app that reads storage locations and displays them pinned on a map.',
-      link: 'https://github.com/DevKevs/flutter-QRScaner'
+      link: 'https://github.com/DevKevs/flutter-QRScaner',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'Node.js ToDo API',
       fieldText: 'Node.js API for a ToDo app with JWT-based login authentication.',
-      link: 'https://github.com/DevKevs/Node-Auth-Task-API'
+      link: 'https://github.com/DevKevs/Node-Auth-Task-API',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
-      field: 'Demo - Expense Tracker',
+      field: 'Expense Tracker',
       fieldText: 'Vue Expense Tracker app for manage income and outcome transactions.',
-      link: 'https://6566312f892ed2007979ecf7--ultimate-expense-tacker.netlify.app/'
+      link: 'https://ultimate-expense-tacker.netlify.app/',
+      btnText: 'View Demo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'Orders Management',
       fieldText: 'Angular web application for managing restaurant orders.',
-      link: 'https://github.com/DevKevs/MUJ-APP'
+      link: 'https://github.com/DevKevs/MUJ-APP',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'Blazor Cinema Management',
       fieldText: 'Blazor WebAssembly application designed for efficient management of movie bookings.',
-      link: 'https://github.com/DevKevs/Cinema-management'
+      link: 'https://github.com/DevKevs/Cinema-management',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'Python & MongoDb API',
       fieldText: 'CRUD API for the management of diferent business information.',
-      link: 'https://github.com/DevKevs/PyMongo-BussinessAPI'
+      link: 'https://github.com/DevKevs/PyMongo-BussinessAPI',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'Secrets Keeper',
       fieldText: 'Ionic framework app with SQLite for securely storing your personal secrets.',
-      link: 'https://github.com/DevKevs/SecretsApp'
+      link: 'https://github.com/DevKevs/SecretsApp',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'Mobile Periodic Table',
       fieldText: 'Ionic framework app showcasing the elements of the periodic table.',
-      link: 'https://github.com/DevKevs/Periodic-Table-App'
+      link: 'https://github.com/DevKevs/Periodic-Table-App',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'Blazor E-Comerce',
       fieldText: 'Blazor WebAssembly application with admin panel for managin products and client side business logic',
-      link: 'https://github.com/DevKevs/FINAL-PROG3'
+      link: 'https://github.com/DevKevs/FINAL-PROG3',
+      btnText: 'View Repo'
     },
     {
       image: '../../../../assets/images/svg/git.svg',
       field: 'Document Control System',
       fieldText: 'ASP.NET MVC web application designed for document control management.',
-      link: 'https://github.com/DevKevs/Document-Control-System'
+      link: 'https://github.com/DevKevs/Document-Control-System',
+      btnText: 'View Repo'
     },
   ];
 }
